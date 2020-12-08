691f70629f6a6728f2d859cb2c626d81ccdba164
Dziuda.Alicja <215713@edu.p.lodz.pl>
2020-12-08 14:29:29 +0100

    constructor in Endpoint Class is done

--- src/main/java/com/example/demo/endpoints/Endpoint.java
+++ src/main/java/com/example/demo/endpoints/Endpoint.java
@@ -1,5 +1,15 @@
 package com.example.demo.endpoints;
 
-public class Endpoint {
+import com.example.demo.services.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
+public class Endpoint {
+    private Service service;
+    //wstrzykiwanie obiektu Service
+    @Autowired
+    public Endpoint(Service service) {
+        this.service = service;
+    }
 }
--- src/main/java/com/example/demo/services/Service.java
+++ src/main/java/com/example/demo/services/Service.java
@@ -1,5 +1,5 @@
 package com.example.demo.services;
-
+@org.springframework.stereotype.Service
 public class Service {
 
 }
