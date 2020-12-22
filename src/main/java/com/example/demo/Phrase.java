94b5aafeaad5002ab4c56bafcdad9275aa142941
Dziuda.Alicja <215713@edu.p.lodz.pl>
2020-12-22 14:53:43 +0100

    Phrase class has page counter for pagination

--- src/main/java/com/example/demo/Phrase.java
+++ src/main/java/com/example/demo/Phrase.java
@@ -2,7 +2,7 @@ package com.example.demo;
 //klasa symbolizująca wpisaną frazę w wyszukiwarkę
 public class Phrase {
     String phrase;
-
+    int page;
     public Phrase() { }
 
     public String getPhrase() {
@@ -11,5 +11,13 @@ public class Phrase {
 
     public void setPhrase(String phrase) {
         this.phrase = phrase;
+    }
+    
+    public int getPage(){
+        return page;
+    }
+
+    public void setPage(int page){
+        this.page = page;
     }
 }
